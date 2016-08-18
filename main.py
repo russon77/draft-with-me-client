4b987b76acdf5f187548b9f801363f84ede59967
Tristan K <russon77@gmail.com>
2016-08-18 13:55:51 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,134 @@
+from threading import Thread
+from time import sleep
+
+from hearthstonecarddetector import card_image_to_id
+from hearthstonearenalogwatcher import HearthstoneArenaLogWatcher, ArenaEvent
+from utils import get_hearthstone_window, get_cards_from_arena_image
+
+import tkinter
+import requests
+
+
+BACKEND_URL = "https://cryptic-harbor-31880.herokuapp.com/"
+SLEEP_ANIMATION_TIMER = 2
+
+
+def update_cards_on_server(card_images, session_data):
+    update_data_on_server(session_data, [card_image_to_id(c) for c in card_images], "cards")
+
+
+def update_data_on_server(session_data, data, name):
+    target_url = BACKEND_URL + "session/update/" + name + "/" + session_data["session_id"]
+
+    r = requests.post(target_url, json={**session_data, name: data})
+
+    if not r.ok:
+        # todo error handling again, like try again? -- but not indefinitely. maybe fail here?
+        # update_data_on_server(session_data, data, name)
+        pass
+
+
+def main(application):
+    halw = HearthstoneArenaLogWatcher()
+
+    print("Contacting server...")
+
+    r = requests.get(BACKEND_URL + "session/new")
+
+    if not r.ok:
+        application.update_status("Unable to contact server!")
+        print("Unable to contact server!")
+        raise ValueError
+
+    session_data = r.json()
+
+    print("Got session id from server.")
+    application.update_status("Contacted server!")
+    application.update_url(BACKEND_URL + "viewer/" + session_data["session_id"])
+
+    for event in halw.event_generator():
+
+        if event.type == ArenaEvent.ENTERED_ARENA or \
+                event.type == ArenaEvent.CARD_DRAFTED:
+            # update server with current state
+            current_state = event.data
+
+            print("Uploading initial status...")
+            application.update_status("Uploading current status...")
+
+            # if hero is set, then draft has started and we can process the cards on screen
+            if current_state.hero:
+                sleep(SLEEP_ANIMATION_TIMER)
+                update_cards_on_server(get_cards_from_arena_image(get_hearthstone_window()), session_data)
+
+                # we also should update the server with the hero and any drafted cards currently selected
+                update_data_on_server(session_data, current_state.hero, "hero")
+                update_data_on_server(session_data, current_state.drafted, "drafted")
+
+            print("Finished uploading current status!")
+            application.update_status("Finished uploading current status!")
+
+        elif event.type == ArenaEvent.DRAFT_ENDED:
+            # update server with drafted
+            print("Uploading final status...")
+            application.update_status("Uploading final status...")
+
+            update_data_on_server(session_data, event.data.drafted, "drafted")
+
+            print("Updated final status on server. Finished draft!")
+            application.update_status("Updated final status on server. Finished draft!")
+
+
+class MyApp(object):
+    def __init__(self, master):
+        self.master = master
+        master.minsize(height=40, width=220)
+
+        master.title("Draft With Me")
+        master.resizable(False, False)
+
+        # frame encompassing entire window
+        overlord_frame = tkinter.Frame(master)
+        overlord_frame.pack()
+
+        # frame encompassing just the Status and Status-Label
+        self.status_frame = tkinter.Frame(overlord_frame)
+        self.status_frame.pack()
+
+        self.static_status_label = tkinter.Label(self.status_frame, text="Status: ")
+        self.static_status_label.pack(side=tkinter.LEFT)
+
+        self.status_label = tkinter.Label(self.status_frame, text="Loading...")
+        self.status_label.pack(side=tkinter.LEFT)
+
+        # frame encompassing the Link and Link-Label
+        self.link_frame = tkinter.Frame(overlord_frame)
+        self.link_frame.pack()
+
+        self.link_label = tkinter.Label(self.link_frame, text="URL: ")
+        self.link_label.pack(side=tkinter.LEFT)
+
+        self.link_entry = tkinter.Entry(self.link_frame)
+        self.link_entry.insert(0, "Waiting...")
+        self.link_entry.configure(state="readonly")
+        self.link_entry.pack(side=tkinter.LEFT)
+
+        # set up and start thread for processing game and getting images
+        thr = Thread(target=main, args=(self, ), daemon=True)
+        thr.start()
+
+    def update_status(self, new_status):
+        self.status_label.configure(text=new_status)
+
+    def update_url(self, new_url):
+        self.link_entry.configure(state=tkinter.NORMAL)
+        self.link_entry.delete(0, tkinter.END)
+        self.link_entry.insert(0, new_url)
+
+
+if __name__ == '__main__':
+    root = tkinter.Tk()
+
+    app = MyApp(root)
+
+    root.mainloop()
---
+++ utils.py
@@ -0,0 +1,72 @@
+from PIL import ImageGrab
+from sys import platform as _platform
+
+
+class WindowNotFoundException(Exception):
+    pass
+
+
+def get_cards_from_arena_image(img):
+    w, h = img.size
+    aspect_ratio = w / h
+    max_ratio = 1.77
+    min_ratio = 1.33
+    x_modifier = (aspect_ratio - min_ratio) / (max_ratio - min_ratio)
+
+    card_bounds = [
+        (w * (0.115 + (0.10 * x_modifier)), h * 0.25, w * (0.26 + (0.06 * x_modifier)), h * 0.53),
+        (w * (0.305 + (0.05 * x_modifier)), h * 0.25, w * 0.46, h * 0.53),
+        (w * 0.495, h * 0.25, w * (0.645 - (0.04 * x_modifier)), h * 0.53)
+    ]
+
+    cropped_images = [img.crop(c) for c in card_bounds]
+
+    # we are further cropping the image -- to get just the artwork
+    card_width, card_height = cropped_images[0].size
+    bbox = 0, 0, card_width, 0.46 * card_height
+    cropped_images = [c.crop(bbox) for c in cropped_images]
+
+    # debug -- delete this!
+    # img.save("tmp/window.png")
+    # for i in range(0, len(cropped_images)):
+    #     cropped_images[i].save("tmp/%d.png" % i)
+
+    return cropped_images
+
+# to implement our polyfill, define a different method for each platform
+# thanks to http://stackoverflow.com/questions/8220108/how-do-i-check-the-operating-system-in-python/8220141#8220141
+if _platform == "linux" or _platform == "linux2":
+    # todo linux
+    pass
+elif _platform == "darwin":
+    # todo os x
+    pass
+elif _platform == "win32":
+    import win32gui
+
+
+    def window_enum_callback(hwnd, extras):
+        rect = win32gui.GetWindowRect(hwnd)
+        x = rect[0]
+        y = rect[1]
+        if win32gui.GetWindowText(hwnd) == "Hearthstone":
+            # tmp = ImageGrab.grab()
+            extras["image"] = ImageGrab.grab((x, y, rect[2], rect[3]))
+            # extras["image"] = tmp.crop((x, y, rect[2], rect[3]))
+            # debug -- delete me!
+            # extras["image"].save("tmp/window_initial.png")
+            return
+
+    def get_hearthstone_log_folder():
+        # todo fill me in across the board
+        pass
+
+    def get_hearthstone_window():
+        extras = {"image": None}
+        win32gui.EnumWindows(window_enum_callback, extras)
+
+        if extras["image"]:
+            return extras["image"]
+
+        raise WindowNotFoundException
+
